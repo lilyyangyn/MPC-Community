e7e24f9c5b035ee6b892af58cb67b3a6e94cd548
lilyyangyn <yyn605@outlook.com>
2023-01-15 15:34:38 +0100

    perf test update

--- peer/tests/perf/benchmark_blockchain_test.go
+++ peer/tests/perf/benchmark_blockchain_test.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/rs/zerolog"
+	"github.com/stretchr/testify/require"
 	"go.dedis.ch/cs438/peer/tests"
 	"go.dedis.ch/cs438/permissioned-chain"
 	"go.dedis.ch/cs438/transport/channel"
@@ -61,6 +62,39 @@ func Test_Perf_BC_Troughput(t *testing.T) {
 			}
 
 			fmt.Printf("Result: Count=%f, Commit=%f\n", sentNum/float64(repeat), commitNum/float64(repeat))
+		}
+	}
+
+	t.Run("small group", getTest(3, 10, "2s", 5))
+	// t.Run("medium group", getTest(5, 10, "2s", 10))
+	// t.Run("large group", getTest(100, 10, "2s", 10))
+}
+
+func Test_Perf_BC_Troughput_2(t *testing.T) {
+	zerolog.SetGlobalLevel(zerolog.ErrorLevel)
+	getTest := func(n int, maxTxn int, blkTimeout string, repeat int) func(*testing.T) {
+		return func(t *testing.T) {
+			nodes, addrs := tests.Setup_n_peers_bc_helper(t, channel.NewTransport(), n, maxTxn, blkTimeout,
+				[]float64{10000}, true, true)
+			fmt.Println("----------set up correctly----------")
+			nodeA := nodes[0]
+			addrA := addrs[0]
+
+			start := time.Now()
+
+			for j := 0; j < 1000; j++ {
+				err := nodeA.SetValueDBAsset("a", 1, float64(j))
+				require.NoError(t, err)
+				time.Sleep(time.Microsecond * 400)
+			}
+
+			elapsed := time.Since(start)
+
+			block := nodeA.BCGetLatestBlock()
+			record := permissioned.GetAssetsFromWorldState(block.States, addrA)
+			commit := record.Assets["a"]
+
+			fmt.Printf("Benchmark took %s. Sent: 1000, Commit: %f \n", elapsed, commit)
 		}
 	}
 
